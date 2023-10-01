6dd55dbdc93020586bd899bf1517522e8e2ed16e
Agustin <agustincivano@gmail.com>
2023-10-01 11:20:06 -0300

    jwt

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,9 +15,9 @@ const api = Router()
 
 //agregar jwtVerifyAuthToken,
 api.use('/products',jwtVerifyAuthToken,ProductRouter)
-api.use('/user',UserRouter)
-api.use('/carts',CartRouter)
-api.use('/ticket',CartRouter)
+api.use('/user',jwtVerifyAuthToken,UserRouter)
+api.use('/carts',jwtVerifyAuthToken,CartRouter)
+api.use('/ticket',jwtVerifyAuthToken,CartRouter)
 
 api.use('/notification', NotificationRoutes)
 
